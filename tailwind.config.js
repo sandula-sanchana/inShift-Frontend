a0d79d5a6639f2d37c0c79c42a3556c34fa7fbff
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-07 17:35:38 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shift: {
+          primary: '#6366f1', // Indigo 500
+          dark: '#4f46e5',
+          light: '#eef2ff'
+        }
+      }
+    },
   },
   plugins: [],
 }
-
