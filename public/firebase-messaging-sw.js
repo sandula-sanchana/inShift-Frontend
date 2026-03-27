90123f724b0f6fa932c64b12a3c30e16db8ddae9
sandula-sanchana <sandulasanchana@gmail.com>
2026-03-27 21:40:14 +0530

    update firebase sw v2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,91 @@
+// /* global importScripts, firebase, clients, self */
+//
+// importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+// importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+//
+// firebase.initializeApp({
+//     apiKey: "AIzaSyBXgUWgOqXOgcfR28_AU5NdzdKSi6iOd7g",
+//     authDomain: "inshift-8b9f0.firebaseapp.com",
+//     projectId: "inshift-8b9f0",
+//     storageBucket: "inshift-8b9f0.firebasestorage.app",
+//     messagingSenderId: "311906880354",
+//     appId: "1:311906880354:web:c77d8c6679baa08db33a3b"
+// });
+//
+// var messaging = firebase.messaging();
+//
+// messaging.onBackgroundMessage(function (payload) {
+//     var title =
+//         payload && payload.notification && payload.notification.title
+//             ? payload.notification.title
+//             : "InShift";
+//
+//     var body =
+//         payload && payload.notification && payload.notification.body
+//             ? payload.notification.body
+//             : "";
+//
+//     var url =
+//         payload && payload.data && payload.data.url
+//             ? payload.data.url
+//             : "/emp/notifications";
+//
+//     var presenceCheckId =
+//         payload && payload.data && payload.data.presenceCheckId
+//             ? payload.data.presenceCheckId
+//             : null;
+//
+//     var sourceExpected =
+//         payload && payload.data && payload.data.sourceExpected
+//             ? payload.data.sourceExpected
+//             : "ANY";
+//
+//     self.registration.showNotification(title, {
+//         body: body,
+//         data: {
+//             url: url,
+//             presenceCheckId: presenceCheckId,
+//             sourceExpected: sourceExpected,
+//             type: payload && payload.data ? payload.data.type : null
+//         }
+//     });
+// });
+//
+// self.addEventListener("notificationclick", function (event) {
+//     event.notification.close();
+//
+//     var url =
+//         event.notification &&
+//         event.notification.data &&
+//         event.notification.data.url
+//             ? event.notification.data.url
+//             : "/emp/notifications";
+//
+//     event.waitUntil(
+//         clients.matchAll({ type: "window", includeUncontrolled: true }).then(function (clientList) {
+//             for (var i = 0; i < clientList.length; i++) {
+//                 var client = clientList[i];
+//
+//                 if (client && "url" in client && client.url && client.url.indexOf(self.location.origin) === 0) {
+//                     if ("navigate" in client) {
+//                         return client.navigate(url).then(function () {
+//                             return client.focus();
+//                         });
+//                     }
+//
+//                     client.postMessage({
+//                         type: "NAVIGATE",
+//                         url: url
+//                     });
+//                     return client.focus();
+//                 }
+//             }
+//
+//             return clients.openWindow(url);
+//         })
+//     );
+// });
+
 /* global importScripts, firebase, clients, self */
 
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
@@ -42,6 +130,7 @@ messaging.onBackgroundMessage(function (payload) {
 
     self.registration.showNotification(title, {
         body: body,
+        requireInteraction: true,
         data: {
             url: url,
             presenceCheckId: presenceCheckId,
